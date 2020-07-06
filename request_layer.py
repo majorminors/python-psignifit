4b26d2b31649206101bd60ce893dbb06b2e8d74e
dorian <dorian@mino.rs>
2020-07-06 21:40:50 +0100

    cleanup for CBU

--- request_layer.py
+++ request_layer.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, render_template, jsonify
+from flask_cors import CORS
 import numpy as np
 import psignifit as ps
 
 app = Flask(__name__, root_path='request_layer/') # make Flask look in the 'request_layer/' dir for templates etc, instead of in the root dir
+CORS(app)
 
-@app.route('/')
-def output(): # serve the demo script - can delete if not needed or point at your experiment file
-    return render_template('demo.html')
+#@app.route('/')
+#def output(): # serve the demo script
+#    return render_template('demo.html')
 
 @app.route("/psignifit", methods=['POST'])
 def calculate():
@@ -16,27 +18,17 @@ def calculate():
         abort(413)
     else: # if JSON, continue
         recieved_data = request.get_json() # pull the data out of the POST request 
-        converted_data = recieved_data['faked_data'] # format of the POST comes in as a dict, so just select the array
+        converted_data = recieved_data['data_array'] # format of the POST comes in as a dict, so just select the array
 
         # set up psignifit with some standard options
         options = dict();
         options['sigmoidName'] = 'norm';
         options['expType']     = '2AFC';
-        # add any options here
-
-        # then run psignifit
-        # result = ps.psignifit(converted_data,options);
 
         options['threshPC']    = 0.9;
         result_upper = ps.psignifit(converted_data,options);
         options['threshPC']    = 0.7;
         result_lower = ps.psignifit(converted_data,options);
-
-        ## example: pull threshold value and return it
-        # threshold = result['Fit'][0]
-        # response = jsonify(threshold) # we must first convert it to JSON
-        # return response # then return the JSON for the axios request to pick up
-        ## we could do the above in one line if we wanted: return jsonify(result['Fit'][0])
 
         thresholds = [result_upper['Fit'][0],result_lower['Fit'][0]]
 
